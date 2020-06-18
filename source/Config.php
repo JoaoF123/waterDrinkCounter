c3a45478e879f0037ca25eaf3bb1ab45da1928f4
João Félix <felixl.joao@gmail.com>
2020-06-18 17:33:59 -0300

    Including heroku data

--- source/Config.php
+++ source/Config.php
@@ -1,8 +1,8 @@
 <?php
 
-    define("BASE_URL", "http://localhost/waterDrinkCounterAPI");
+    define("BASE_URL", "https://waterdrinkcounter.herokuapp.com");
 
-    define("DB_DSN", "mysql:host=127.0.0.1; dbname=waterdrinkcounter; charset=utf8");
-    define("DB_USERNAME", "root");
-    define("DB_PASSWORD", "");
+    define("DB_DSN", "mysql:host=us-cdbr-east-05.cleardb.net; dbname=heroku_7630812ea93cf19; charset=utf8");
+    define("DB_USERNAME", "b9a830548a05fb");
+    define("DB_PASSWORD", "89016ac3");
     define("DB_OPTIONS", [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']);
